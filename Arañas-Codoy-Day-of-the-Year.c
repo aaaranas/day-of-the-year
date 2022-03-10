3f089ec55404248ea1c78329231e55034e1c3f47
Andre Milan A. Arañas <100919391+aaaranas@users.noreply.github.com>
2022-03-10 15:39:50 +0800

    Update Arañas-Codoy-Day-of-the-Year.c

--- Arañas-Codoy-Day-of-the-Year.c
+++ Arañas-Codoy-Day-of-the-Year.c
@@ -103,27 +103,29 @@ int main(){
     
     if (digit == 0)
       printf("Invalid Input.\n");
-    
+
+    if(m>=1 && m<=12){        
+        if((d>=1 && d<=31) && (m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12)){
+                solutionFunction(m,d,y);
+                invalid=1;
+            }
+            else if((d>=1 && d<=30) && (m==4 || m==6 || m==9 || m==11)){
+                solutionFunction(m,d,y);
+                invalid=1;
+            }
+            else if(d==29 && m==2 && (y%400==0 ||(y%4==0 && y%100!=0))){
+                solutionFunction(m,d,y);
+                invalid=1;
+            }
+            else{
+                printf("Invalid input. ");
+            }
+            return 0; 
+    }
     }while(digit==0);
 
-  if(m>=1 && m<=12){        
-    if((d>=1 && d<=31) && (m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12)){
-            solutionFunction(m,d,y);
-            invalid=1;
-        }
-        else if((d>=1 && d<=30) && (m==4 || m==6 || m==9 || m==11)){
-            solutionFunction(m,d,y);
-            invalid=1;
-        }
-        else if(d==29 && m==2 && (y%400==0 ||(y%4==0 && y%100!=0))){
-            solutionFunction(m,d,y);
-            invalid=1;
-        }
-        else{
-            printf("Invalid input. ");
-        }
-    return 0; 
-    }
+
+
 }
 
 int isDigit(char*m, char*d, char*y){ 
